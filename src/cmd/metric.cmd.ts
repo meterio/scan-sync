b8e3cd862001c35196df5e7916ab7d6d79838616
simonzg <zhanghan.simon@gmail.com>
2020-11-25 20:33:06 +0800

    add metric collection for price/change

--- src/cmd/metric.cmd.ts
+++ src/cmd/metric.cmd.ts
@@ -1,9 +1,9 @@
 import { EventEmitter } from 'events';
+import { stringify } from 'querystring';
 
 import * as Logger from 'bunyan';
 
-import { MetricType, Network } from '../const';
-import { Metric } from '../model/metric.interface';
+import { MetricName, MetricType, Network } from '../const';
 import MetricRepo from '../repo/metric.repo';
 import { Net } from '../utils/net';
 import { Pos } from '../utils/pos-rest';
@@ -13,13 +13,17 @@ import { CMD } from './cmd';
 
 const SAMPLING_INTERVAL = 3000;
 const METRIC_DEFS = [
-  { key: 'difficulty', type: MetricType.BIGNUM, default: '1' },
-  { key: 'networkhashps', type: MetricType.BIGNUM, default: '0' },
-  { key: 'epoch', type: MetricType.NUM, default: '0' },
-  { key: 'seq', type: MetricType.NUM, default: '0' },
-  { key: 'kblock', type: MetricType.NUM, default: '0' },
-  { key: 'posbest', type: MetricType.NUM, default: '0' },
-  { key: 'powbest', type: MetricType.NUM, default: '0' },
+  { key: MetricName.DIFFICULTY, type: MetricType.BIGNUM, default: '1' },
+  { key: MetricName.HASHRATE, type: MetricType.BIGNUM, default: '0' },
+  { key: MetricName.EPOCH, type: MetricType.NUM, default: '0' },
+  { key: MetricName.SEQ, type: MetricType.NUM, default: '0' },
+  { key: MetricName.KBLOCK, type: MetricType.NUM, default: '0' },
+  { key: MetricName.POS_BEST, type: MetricType.NUM, default: '0' },
+  { key: MetricName.POW_BEST, type: MetricType.NUM, default: '0' },
+  { key: MetricName.MTRG_PRICE, type: MetricType.NUM, default: '1' },
+  { key: MetricName.MTRG_PRICE_CHANGE, type: MetricType.STRING, default: '0%' },
+  { key: MetricName.MTR_PRICE, type: MetricType.NUM, default: '0.5' },
+  { key: MetricName.MTR_PRICE_CHANGE, type: MetricType.STRING, default: '0%' },
 ];
 
 class MetricCache {
@@ -43,6 +47,7 @@ class MetricCache {
     if (key in this.map && value !== undefined) {
       if (value != this.map[key]) {
         this.map[key] = value;
+        console.log('update ', key, 'to ', value);
         await this.metricRepo.update(key, value);
       }
     }
@@ -57,6 +62,7 @@ export class MetricCMD extends CMD {
   private metricRepo = new MetricRepo();
   private pos: Pos;
   private pow = new Pow();
+  private coingecko = new Net('https://api.coingecko.com/api/v3/');
 
   private cache = new MetricCache();
 
@@ -93,6 +99,11 @@ export class MetricCMD extends CMD {
   }
 
   public async loop() {
+    let index = 0;
+    const reset24h = (3600 * 24) / (SAMPLING_INTERVAL / 1000); // count of index in 24 hours
+    const reset30s = 30 / (SAMPLING_INTERVAL / 1000); // count of index in 30 seconds
+    const reset1m = 60 / (SAMPLING_INTERVAL / 1000); // count of index in 1 minute
+    const reset5m = (60 * 5) / (SAMPLING_INTERVAL / 1000); // count of index in 5 minutes
     for (;;) {
       try {
         if (this.shutdown) {
@@ -104,19 +115,42 @@ export class MetricCMD extends CMD {
         // update difficulty && hps
         const mining = await this.pow.getMiningInfo();
         if (!!mining) {
-          await this.cache.update('difficulty', mining.difficulty);
-          await this.cache.update('networkhashps', mining.networkhashps);
-          await this.cache.update('powbest', mining.blocks);
+          await this.cache.update(MetricName.DIFFICULTY, mining.difficulty);
+          await this.cache.update(MetricName.HASHRATE, mining.networkhashps);
+          await this.cache.update(MetricName.POW_BEST, mining.blocks);
         }
 
         // update epoch && round
         const blk = await this.pos.getBlock('best', 'regular');
         if (!!blk) {
           const seq = blk.number - blk.lastKBlockHeight;
-          await this.cache.update('posbest', String(blk.number));
-          await this.cache.update('kblock', String(blk.lastKBlockHeight));
-          await this.cache.update('seq', String(seq));
+          await this.cache.update(MetricName.POS_BEST, String(blk.number));
+          await this.cache.update(MetricName.KBLOCK, String(blk.lastKBlockHeight));
+          await this.cache.update(MetricName.SEQ, String(seq));
         }
+
+        // update every minute
+        if (index % reset1m === 0) {
+          const price = await this.coingecko.http<any>('GET', 'simple/price', {
+            query: { ids: 'meter,meter-stable', vs_currencies: 'usd,usd', include_24hr_change: 'true' },
+          });
+          if (!!price) {
+            if (price.meter) {
+              const m = price.meter;
+              const percent20h = Math.floor(parseFloat(m.usd_24h_change) * 100) / 100;
+              this.cache.update(MetricName.MTRG_PRICE, String(m.usd));
+              this.cache.update(MetricName.MTRG_PRICE_CHANGE, `${percent20h}%`);
+            }
+            if (price['meter-stable']) {
+              const m = price['meter-stable'];
+              const percent20h = Math.floor(parseFloat(m.usd_24h_change) * 100) / 100;
+              this.cache.update(MetricName.MTR_PRICE, String(m.usd));
+              this.cache.update(MetricName.MTR_PRICE_CHANGE, `${percent20h}%`);
+            }
+          }
+        }
+
+        index = (index + 1) % reset24h; // clear up 24hours
       } catch (e) {
         if (!(e instanceof InterruptedError)) {
           this.logger.error(this.name + 'loop: ' + (e as Error).stack);
--- src/const/model.ts
+++ src/const/model.ts
@@ -15,4 +15,17 @@ export enum MetricType {
   STRING,
 }
 
+export enum MetricName {
+  DIFFICULTY = 'difficulty',
+  HASHRATE = 'networkhashps',
+  EPOCH = 'epoch',
+  SEQ = 'seq',
+  KBLOCK = 'kblock',
+  POS_BEST = 'pos_best',
+  POW_BEST = 'pow_best',
+  MTRG_PRICE = 'mtrg_price',
+  MTRG_PRICE_CHANGE = 'mtrg_price_change',
+  MTR_PRICE = 'mtr_price',
+  MTR_PRICE_CHANGE = 'mtr_price_change',
+}
 export const enumKeys = (es: any) => Object.values(es).filter((x) => typeof x === 'string');
