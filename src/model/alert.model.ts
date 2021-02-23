3b8eba249b2df50277823d6ef7df3c9494353cc7
simonzg <zhanghan.simon@gmail.com>
2021-02-23 21:31:01 +0800

    v0.2.4

--- src/model/alert.model.ts
+++ src/model/alert.model.ts
@@ -4,7 +4,7 @@ import { Alert } from './alert.interface';
 
 const alertSchema = new mongoose.Schema(
   {
-    network: { type: String, enum: ['mainnet', 'testnet'], required: true },
+    network: { type: String, enum: ['mainnet', 'testnet', 'devnet'], required: true },
     number: { type: Number, required: true },
     epoch: { type: Number, required: true },
     channel: { type: String, enum: ['slack'], required: true },
@@ -19,6 +19,7 @@ const alertSchema = new mongoose.Schema(
   }
 );
 
+alertSchema.index({ network: 1, number: 1, epoch: 1, channel: 1, msg: 1 });
 alertSchema.set('toJSON', {
   transform: (doc, ret, options) => {
     delete ret.__v;
