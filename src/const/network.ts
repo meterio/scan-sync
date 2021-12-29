1b30422b52f2a214f8a2f50bc25808e8339f5aa4
kda-data <kda-data@protonmail.com>
2021-12-29 09:59:13 +0800

    update warringstakes genesis

--- src/const/network.ts
+++ src/const/network.ts
@@ -1,6 +1,6 @@
 export enum Network {
   MainNet = '0x000000008d23443cdb7c8b00391d41d957eab0d5bf812a1657f73f75ead5f997', // mainnet
-  TestNet = '0x0000000066429e45f4fac1771b85c839ccbc9bece7c03bdf9003e295739dd6e0', // warringstakes
+  TestNet = '0x000000000a03bf523d73774b881c66fbc91df6a0e139a312376676ddd734a4b1', // warringstakes
   DevNet = '0x00000000ed77a5a4cc2cb585ed7fba4200b89751142cd6fe124aecc3d3350e58', // tetra
 }
 
