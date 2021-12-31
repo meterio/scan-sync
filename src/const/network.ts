8132e95c4726c7c22279fdc4c7b1d4aa03a846c5
kda-data <kda-data@protonmail.com>
2021-12-31 17:20:16 +0800

    MainNet

--- src/const/network.ts
+++ src/const/network.ts
@@ -1,5 +1,5 @@
 export enum Network {
-  MainNet = '0x0000000007be693a0f0b9120a60965880a9dbb30f9bc1358e167f5df97e650db', // mainnet
+  MainNet = '0x00000000b08e2405eb4f5b45187746c64fe1171ae640730ece343757b4bf415e', // mainnet
   TestNet = '0x00000000d87b1c4973d56b0b9d6b3f24c85dccd74034724dfc5c1b4682aa3c6f', // warringstakes
   DevNet = '0x00000000ed77a5a4cc2cb585ed7fba4200b89751142cd6fe124aecc3d3350e58', // tetra
 }
