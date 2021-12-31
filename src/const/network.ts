fff35ebe53b4ddaeca717ed6e70496915f500a1b
kda-data <kda-data@protonmail.com>
2021-12-31 15:16:40 +0800

    update testNet genesis

--- src/const/network.ts
+++ src/const/network.ts
@@ -1,6 +1,6 @@
 export enum Network {
   MainNet = '0x0000000007be693a0f0b9120a60965880a9dbb30f9bc1358e167f5df97e650db', // mainnet
-  TestNet = '0x000000000a03bf523d73774b881c66fbc91df6a0e139a312376676ddd734a4b1', // warringstakes
+  TestNet = '0x00000000d87b1c4973d56b0b9d6b3f24c85dccd74034724dfc5c1b4682aa3c6f', // warringstakes
   DevNet = '0x00000000ed77a5a4cc2cb585ed7fba4200b89751142cd6fe124aecc3d3350e58', // tetra
 }
 
