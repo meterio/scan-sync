6e57ccfa374c0521b2bf4b423a946f5c17551a61
kda-data <kda-data@protonmail.com>
2022-01-03 15:12:33 +0800

    testNet genesis

--- src/const/network.ts
+++ src/const/network.ts
@@ -1,6 +1,6 @@
 export enum Network {
   MainNet = '0x00000000b08e2405eb4f5b45187746c64fe1171ae640730ece343757b4bf415e', // mainnet
-  TestNet = '0x00000000c84aab958ca2b62571fbeb1c891ae5c1c505283e04e84e6d0e844440', // testnet
+  TestNet = '0x000000008b54f747574b4d3e3297c3280e9f30a09b411232b817416ef477a7ec', // testnet
   DevNet = '0x00000000ed77a5a4cc2cb585ed7fba4200b89751142cd6fe124aecc3d3350e58', // tetra
 }
 
