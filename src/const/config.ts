e2c267ebaea83aa3807929c32091c8ac53219959
kda-data <kda-data@protonmail.com>
2022-01-14 14:59:46 +0800

    SYSTEM_TOKEN_SYMBOL from VERSE => STPD

--- src/const/config.ts
+++ src/const/config.ts
@@ -2,4 +2,4 @@ export const MTRGSysContratAddr = '0x228ebBeE999c6a7ad74A6130E81b12f9Fe237Ba3'.t
 export const MTRSysContratAddr = '0x687A6294D0D6d63e751A059bf1ca68E4AE7B13E2'.toLowerCase();
 
 export const SYSTEM_COIN_SYMBOL = 'STPT'
-export const SYSTEM_TOKEN_SYMBOL = 'VERSE'
+export const SYSTEM_TOKEN_SYMBOL = 'STPD'
