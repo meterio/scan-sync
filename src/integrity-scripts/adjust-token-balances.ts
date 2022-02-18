235f12767f0f45d564ad84ebdeff9ed6c4f29156
Simon Zhang <zhanghan.simon@gmail.com>
2022-02-18 14:47:54 -0800

    add script to adjust token balance

---
+++ src/integrity-scripts/adjust-token-balances.ts
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+require('../utils/validateEnv');
+
+import { abi } from '@meterio/devkit';
+import { blake2b256 } from '@meterio/devkit/dist/cry';
+import BigNumber from 'bignumber.js';
+import mongoose from 'mongoose';
+
+import { Token } from '../const';
+import HeadRepo from '../repo/head.repo';
+import TokenBalanceRepo from '../repo/tokenBalance.repo';
+import { Pos, checkNetworkWithDB, getNetworkFromCli } from '../utils';
+import { connectDB } from '../utils/db';
+
+const balanceOfABI: abi.Function.Definition = {
+  inputs: [{ name: 'account', type: 'address' }],
+  name: 'balanceOf',
+  outputs: [{ name: '', type: 'uint256' }],
+  stateMutability: 'view',
+  type: 'function',
+  payable: false,
+};
+
+const auditTokenBalances = async () => {
+  const net = getNetworkFromCli();
+  if (!net) {
+    process.exit(-1);
+  }
+
+  await connectDB(net);
+  const headRepo = new HeadRepo();
+  const tokenBalanceRepo = new TokenBalanceRepo();
+  const pos = new Pos(net);
+  await checkNetworkWithDB(net);
+
+  const balances = await tokenBalanceRepo.findAll();
+  console.log('start checking...');
+
+  const balanceOfFunc = new abi.Function(balanceOfABI);
+  for (const bal of balances) {
+    const { address, tokenAddress, symbol, balance } = bal;
+    try {
+      const outputs = await pos.explain(
+        {
+          clauses: [{ to: tokenAddress, value: '0x0', data: balanceOfFunc.encode(address), token: Token.MTR }],
+        },
+        'best'
+      );
+      const decoded = balanceOfFunc.decode(outputs[0].data);
+      const chainBal = new BigNumber(decoded[0]);
+      if (!chainBal.isEqualTo(balance)) {
+        console.log(`found NON-matching balance: chain ${chainBal} db:${balance}`);
+        console.log(`tokenAddr: ${tokenAddress}, addr: ${address}, symbol: ${symbol}`);
+        bal.balance = chainBal;
+        console.log(`updated`);
+        console.log('----------------------------------------');
+        await bal.save();
+      }
+    } catch {
+      continue;
+    }
+  }
+};
+
+(async () => {
+  try {
+    await auditTokenBalances();
+    await mongoose.disconnect();
+  } catch (e) {
+    console.log(`error: ${e.name} ${e.message} - ${e.stack}`);
+    process.exit(-1);
+  }
+})();
