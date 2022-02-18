a9aac13c90ed2e1ace975eb54f30ca48cd4312e6
Simon Zhang <zhanghan.simon@gmail.com>
2022-02-18 13:58:24 -0800

    add script to adjust total supply

---
+++ src/integrity-scripts/adjust-total-supply.ts
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+require('../utils/validateEnv');
+
+import BigNumber from 'bignumber.js';
+import mongoose from 'mongoose';
+
+import { Token, prototype, totalSupply } from '../const';
+import TokenProfileRepo from '../repo/tokenProfile.repo';
+import { Pos, checkNetworkWithDB, fromWei, getNetworkFromCli } from '../utils';
+import { connectDB } from '../utils/db';
+
+const adjustTotalSupply = async () => {
+  const net = getNetworkFromCli();
+  if (!net) {
+    process.exit(-1);
+  }
+
+  await connectDB(net);
+  const tokenProfileRepo = new TokenProfileRepo();
+  const pos = new Pos(net);
+  await checkNetworkWithDB(net);
+
+  const profiles = await tokenProfileRepo.findAll();
+  console.log('start checking...');
+  for (const p of profiles) {
+    const ret = await pos.explain(
+      { clauses: [{ to: p.address, value: '0x0', data: totalSupply.encode(), token: Token.MTR }] },
+      'best'
+    );
+    const decoded = totalSupply.decode(ret[0].data);
+    const amount = decoded['0'];
+    let updated = false;
+    if (!p.totalSupply.isEqualTo(amount)) {
+      console.log(`Update total supply for token ${p.symbol} from ${p.totalSupply.toFixed(0)} to ${amount}`);
+      p.totalSupply = new BigNumber(amount);
+      updated = true;
+    }
+    if (!p.circulation.isEqualTo(amount)) {
+      console.log(`Update circulation for token ${p.symbol} from ${p.circulation.toFixed(0)} to ${amount}`);
+      p.circulation = new BigNumber(amount);
+      updated = true;
+    }
+    if (updated) {
+      await p.save();
+    }
+  }
+};
+
+(async () => {
+  try {
+    await adjustTotalSupply();
+    await mongoose.disconnect();
+  } catch (e) {
+    console.log(`error: ${e.name} ${e.message} - ${e.stack}`);
+    process.exit(-1);
+  }
+})();
