b4b985e257da29a8a20d05a2f9efbb8f0fd9c79b
Simon Zhang <zhanghan.simon@gmail.com>
2022-08-04 14:51:25 -0400

    update script for update code match contract

--- src/integrity-scripts/correct-code-match-contract.ts
+++ src/integrity-scripts/correct-code-match-contract.ts
@@ -12,16 +12,15 @@ const run = async () => {
   const contractRepo = new ContractRepo();
   await checkNetworkWithDB(network);
 
-  const contracts = await contractRepo.findCodeMatchVerifiedContracts();
+  const contracts = await contractRepo.findCodeMatchVerifiedContract();
   for (const c of contracts) {
+    console.log('correct contract: ', c.address);
     if (c.verified && c.verifiedFrom) {
-      var root: Contract;
-      do {
-        root = await contractRepo.findByAddress(c.verifiedFrom);
-      } while (root.status != 'match');
-      if (root.address !== c.verifiedFrom) {
-        console.log(`update verifiedFrom on ${c.addresss} from ${c.verifiedFrom} to ${root.address}`);
-        c.verifiedFrom = root.address;
+      const root = await contractRepo.findByAddress(c.verifiedFrom);
+      if (root.status === 'match') {
+        const realRoot = await contractRepo.findVerifiedContractsWithCreationInputHash(c.creationInputHash);
+        console.log(`update verifiedFrom on ${c.address} from ${c.verifiedFrom} to ${realRoot.address}`);
+        c.verifiedFrom = realRoot.address;
         await c.save();
       }
     }
