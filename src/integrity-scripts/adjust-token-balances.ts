e5c45915ab8a1630fd842691bd3f51e9cd435877
Simon.Z <zhanghan.simon@gmail.com>
2022-09-27 09:47:25 -0400

    update token balance adjust ment

--- src/integrity-scripts/adjust-token-balances.ts
+++ src/integrity-scripts/adjust-token-balances.ts
@@ -1,51 +1,45 @@
 #!/usr/bin/env node
 require('../utils/validateEnv');
 
-import { abi } from '@meterio/devkit';
-import { BigNumber, HeadRepo, Token, TokenBalanceRepo, connectDB, disconnectDB } from '@meterio/scan-db/dist';
+import { ERC20 } from '@meterio/devkit';
+import { BigNumber, Token, TokenBalanceRepo, connectDB, disconnectDB } from '@meterio/scan-db/dist';
 
 import { Pos, checkNetworkWithDB, getNetworkFromCli } from '../utils';
-
-const balanceOfABI: abi.Function.Definition = {
-  inputs: [{ name: 'account', type: 'address' }],
-  name: 'balanceOf',
-  outputs: [{ name: '', type: 'uint256' }],
-  stateMutability: 'view',
-  type: 'function',
-  payable: false,
-};
 
 const auditTokenBalances = async () => {
   const { network, standby } = getNetworkFromCli();
 
   await connectDB(network, standby);
-  const headRepo = new HeadRepo();
   const tokenBalanceRepo = new TokenBalanceRepo();
   const pos = new Pos(network);
   await checkNetworkWithDB(network);
 
-  const balances = await tokenBalanceRepo.findAll();
+  const balances = await tokenBalanceRepo.findByTokenAddress('0x160361ce13ec33c993b5cca8f62b6864943eb083');
   console.log('start checking...');
 
-  const balanceOfFunc = new abi.Function(balanceOfABI);
   for (const bal of balances) {
     const { address, tokenAddress, balance } = bal;
     try {
       const outputs = await pos.explain(
         {
-          clauses: [{ to: tokenAddress, value: '0x0', data: balanceOfFunc.encode(address), token: Token.MTR }],
+          clauses: [{ to: tokenAddress, value: '0x0', data: ERC20.balanceOf.encode(address), token: Token.MTR }],
         },
         'best'
       );
-      const decoded = balanceOfFunc.decode(outputs[0].data);
+      const decoded = ERC20.balanceOf.decode(outputs[0].data);
       const chainBal = new BigNumber(decoded[0]);
       if (!chainBal.isEqualTo(balance)) {
         console.log(`found NON-matching balance: chain ${chainBal} db:${balance}`);
         console.log(`tokenAddr: ${tokenAddress}, addr: ${address}`);
-        bal.balance = chainBal;
-        console.log(`updated`);
+        if (chainBal.isEqualTo(0)) {
+          await bal.delete();
+          console.log('deleted');
+        } else {
+          bal.balance = chainBal;
+          await bal.save();
+          console.log(`updated`);
+        }
         console.log('----------------------------------------');
-        await bal.save();
       }
     } catch {
       continue;
